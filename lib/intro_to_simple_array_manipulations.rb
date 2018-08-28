a05d39281d62941a6358e497ea76adbb5916a800
Rob Parker <into.the.parker@gmail.com>
2018-08-28 15:10:05 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, string)
+  array.push(string)
+  return array
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+  return array
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array, number)
+  return array.shift(number)
+end
+
+def using_concat(array_one, array_two)
+end
