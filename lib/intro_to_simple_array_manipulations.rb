68c3e85dc50800108794fdfc962c8ae595184c9b
Rob Parker <into.the.parker@gmail.com>
2018-08-28 15:11:42 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,9 +20,10 @@ def using_shift(array)
   return array.shift
 end
 
-def shift_with_args(array, number)
-  return array.shift(number)
+def shift_with_args(array)
+  return array.shift(2)
 end
 
 def using_concat(array_one, array_two)
+  return array_one.concat(array_two)
 end
